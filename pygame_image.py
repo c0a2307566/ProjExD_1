fe4754e84558a5ae3b8ea0a762c98a7cbf13809a
c0a2307566 <c0a2307566@edu.teu.ac.jp>
2024-09-24 16:43:11 +0900

    こうかとんの動きが背景と一致するように変更

--- pygame_image.py
+++ pygame_image.py
@@ -29,7 +29,8 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
+        kk_rct.move_ip((-1, 0))
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bgflip_img, [-x+1600, 0])
